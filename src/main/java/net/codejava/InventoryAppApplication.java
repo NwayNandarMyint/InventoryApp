bbc6f09d559d92562da8deefe2581ecc8ca308db
Aung Kyaw Nyein <aungkyawnyein456@gmail.com>
2024-05-25 21:38:33 +0630

    that is testing

--- src/main/java/net/codejava/InventoryAppApplication.java
+++ src/main/java/net/codejava/InventoryAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryAppApplication.class, args);
 	}
